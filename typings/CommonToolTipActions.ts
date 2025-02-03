8c6bdc17dbe87599914c9a9c8cd9821d5a9a1c9b
David Reband <david.reband@webanizer.de>
2025-02-03 10:04:34 +0100

    Merge pull request #6957 from BlueWallet/tooltipaccessbility
    ADD: Tooltip for header buttons for accessbility

--- typings/CommonToolTipActions.ts
+++ typings/CommonToolTipActions.ts
@@ -7,6 +7,8 @@ const keys = {
   OpenInBlockExplorer: 'open_in_blockExplorer',
   CopyAmount: 'copyAmount',
   CopyNote: 'copyNote',
+  ManageWallets: 'manageWallets',
+  ImportWallet: 'importWallet',
   HideBalance: 'hideBalance',
   ViewInBitcoin: 'viewInBitcoin',
   ViewInSats: 'viewInSats',
@@ -28,6 +30,12 @@ const icons = {
   },
   Note: {
     iconValue: 'note.text',
+  },
+  ManageWallets: {
+    iconValue: 'slider.horizontal.3',
+  },
+  ImportWallet: {
+    iconValue: 'square.and.arrow.down.on.square',
   },
   ViewInBitcoin: {
     iconValue: 'bitcoinsign.circle',
@@ -67,6 +75,16 @@ export const CommonToolTipActions = {
     id: keys.CopyNote,
     text: loc.transactions.details_copy_note,
     icon: icons.Clipboard,
+  },
+  ManageWallet: {
+    id: keys.ManageWallets,
+    text: loc.wallets.manage_title,
+    icon: icons.ManageWallets,
+  },
+  ImportWallet: {
+    id: keys.ImportWallet,
+    text: loc.wallets.add_import_wallet,
+    icon: icons.ImportWallet,
   },
   HideBalance: {
     id: keys.HideBalance,
