51dfd2b1fef24f43f66dfa434f05b6cd2a2aa89a
David Reband <david.reband@webanizer.de>
2025-02-04 16:48:14 +0100

    FIX; Camera safe area UI background was white.

--- screen/send/ScanQRCode.js
+++ screen/send/ScanQRCode.js
@@ -288,7 +288,7 @@ const ScanQRCode = () => {
   const render = isLoading ? (
     <BlueLoading />
   ) : (
-    <SafeArea>
+    <View>
       {!cameraStatusGranted ? (
         <View style={[styles.openSettingsContainer, stylesHook.openSettingsContainer]}>
           <BlueText>{loc.send.permission_camera_message}</BlueText>
@@ -346,10 +346,10 @@ const ScanQRCode = () => {
         style={styles.backdoorButton}
         onPress={handleInvisibleBackdoorPress}
       />
-    </SafeArea>
+    </View>
   );
 
-  return <View style={styles.root}>{render}</View>;
+  return <SafeArea style={styles.root}>{render}</SafeArea>;
 };
 
 export default ScanQRCode;
