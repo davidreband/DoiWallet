7741f3aa2ec16e6019e9d991a3f1fc8051e03b2c
David Reband <david.reband@webanizer.de>
2025-02-04 15:36:10 +0100

    FIX: If theres no valid file then dont attempt to scan for qr

--- components/CompanionDelegates.tsx
+++ components/CompanionDelegates.tsx
@@ -184,6 +184,9 @@ const CompanionDelegates = () => {
 
         if (fileName && /\.(jpe?g|png)$/i.test(fileName)) {
           try {
+            if (!decodedUrl) {
+              throw new Error(loc.send.qr_error_no_qrcode);
+            }
             const values = await RNQRGenerator.detect({
               uri: decodedUrl,
             });
@@ -201,11 +204,12 @@ const CompanionDelegates = () => {
                 },
               );
             } else {
-              triggerHapticFeedback(HapticFeedbackTypes.NotificationError);
-              presentAlert({ message: loc.send.qr_error_no_qrcode });
+              throw new Error(loc.send.qr_error_no_qrcode);
             }
           } catch (error) {
             console.error('Error detecting QR code:', error);
+            triggerHapticFeedback(HapticFeedbackTypes.NotificationError);
+            presentAlert({ message: loc.send.qr_error_no_qrcode });
           }
         } else {
           DeeplinkSchemaMatch.navigationRouteFor(event, (value: [string, any]) => navigationRef.navigate(...value), {
