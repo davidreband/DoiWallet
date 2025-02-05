854a652b032c3d4f1238b97d076f3bf7f8d4522f
David Reband <david.reband@webanizer.de>
2025-02-05 11:56:40 +0100

    REF: Reuse existing id

--- screen/wallets/Add.tsx
+++ screen/wallets/Add.tsx
@@ -170,7 +170,7 @@ const WalletsAdd: React.FC = () => {
         setSelectedWalletType(newWalletType);
       }
     },
-    [entropy, setParams],
+    [entropy, setParams, words],
   );
 
   const navigateToEntropy = useCallback(() => {
@@ -185,11 +185,7 @@ const WalletsAdd: React.FC = () => {
         text: loc.wallets.add_wallet_seed_length_24,
         menuState: words === 24,
       },
-      {
-        id: 'reset_entropy',
-        text: loc.settings.electrum_reset,
-        destructive: true,
-      },
+      { ...CommonToolTipActions.ResetToDefault, hidden: !entropy },
     ];
 
     const entropyAction: Action = {
@@ -245,7 +241,7 @@ const WalletsAdd: React.FC = () => {
     };
 
     return selectedWalletType === ButtonSelected.ONCHAIN ? [walletAction, navigateToEntropy()] : [walletAction];
-  }, [navigateToEntropy, selectedWalletType]);
+  }, [navigateToEntropy, selectedWalletType, selectedIndex]);
 
   const handleOnLightningButtonPressed = useCallback(() => {
     confirmResetEntropy(ButtonSelected.OFFCHAIN);
@@ -270,14 +266,14 @@ const WalletsAdd: React.FC = () => {
             navigate('ProvideEntropy', { words: 12, entropy: entropy?.toString('hex') });
           } else if (id === '24_words') {
             navigate('ProvideEntropy', { words: 24, entropy: entropy?.toString('hex') });
-          } else if (id === 'reset_entropy') {
+          } else if (id === CommonToolTipActions.ResetToDefault.id) {
             confirmResetEntropy(ButtonSelected.ONCHAIN);
           }
         }}
         actions={toolTipActions}
       />
     ),
-    [handleOnLightningButtonPressed, navigateToEntropy, toolTipActions, entropy],
+    [handleOnLightningButtonPressed, navigateToEntropy, toolTipActions, entropy, confirmResetEntropy, navigate],
   );
 
   useEffect(() => {
