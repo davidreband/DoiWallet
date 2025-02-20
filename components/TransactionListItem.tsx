327c6c1d6272a8c90cd4db04ff44219b81ba2e51
Marcos Rodriguez Velez <marcospr@pm.me>
2025-02-20 19:32:01 -0400

    Revert "ADD: TX Details action action"
    This reverts commit cdd923db7c4e8101f9a5e2cf2545d40b4c42f327.

--- components/TransactionListItem.tsx
+++ components/TransactionListItem.tsx
@@ -368,7 +368,7 @@ export const TransactionListItem: React.FC<TransactionListItemProps> = React.mem
         handleOnViewOnBlockExplorer,
       ],
     );
-    const toolTipActions = useMemo((): Action[] | Action[][] => {
+    const toolTipActions = useMemo((): Action[] => {
       const actions: (Action | Action[])[] = [];
 
       if (rowTitle !== loc.lnd.expired) {
@@ -387,7 +387,7 @@ export const TransactionListItem: React.FC<TransactionListItemProps> = React.mem
         actions.push([CommonToolTipActions.ExpandNote]);
       }
 
-      return actions as Action[] | Action[][];
+      return actions as Action[];
     }, [item.hash, subtitle, rowTitle, subtitleNumberOfLines]);
 
     const accessibilityState = useMemo(() => {
