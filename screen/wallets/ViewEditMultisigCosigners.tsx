2e44e94f4d7dae0cdb7852520f0305b0c586dfee
Marcos Rodriguez Velez <marcospr@pm.me>
2025-02-23 21:15:10 -0400

    wip

--- screen/wallets/ViewEditMultisigCosigners.tsx
+++ screen/wallets/ViewEditMultisigCosigners.tsx
@@ -57,7 +57,6 @@ const ViewEditMultisigCosigners: React.FC = () => {
   const { isBiometricUseCapableAndEnabled } = useBiometrics();
   const { isElectrumDisabled, isPrivacyBlurEnabled } = useSettings();
   const { dispatch, setParams, setOptions } = useExtendedNavigation<NavigationProp>();
-  const openScannerButtonRef = useRef(null);
   const route = useRoute<RouteParams>();
   const { walletID } = route.params;
   const w = useRef(wallets.find(wallet => wallet.getID() === walletID));
@@ -560,7 +559,6 @@ const ViewEditMultisigCosigners: React.FC = () => {
             {!isLoading && (
               <>
                 <AddressInputScanButton
-                  ref={openScannerButtonRef}
                   beforePress={async () => {
                     await provideMnemonicsModalRef.current?.dismiss();
                   }}
