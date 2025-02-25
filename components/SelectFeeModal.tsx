88ddb8a1a5c57fb4653ad36a6377896633d137d0
David Reband <david.reband@webanizer.de>
2025-02-25 14:42:30 +0100

    fix: feeUnit

--- components/SelectFeeModal.tsx
+++ components/SelectFeeModal.tsx
@@ -38,7 +38,7 @@ interface SelectFeeModalProps {
 }
 
 const SelectFeeModal = forwardRef<BottomModalHandle, SelectFeeModalProps>(
-  ({ networkTransactionFees, feePrecalc, feeRate, setCustomFee, setFeePrecalc, feeUnit = BitcoinUnit.BTC }, ref) => {
+  ({ networkTransactionFees, feePrecalc, feeRate, setCustomFee, setFeePrecalc, feeUnit = DoichainUnit.DOI }, ref) => {
     const [customFee, setCustomFeeState] = useState('');
     const feeModalRef = useRef<BottomModalHandle>(null);
     const customModalRef = useRef<BottomModalHandle>(null);
