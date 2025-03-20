cb08a1a356692e43feb27cd51032e5e640bc20bd
Marcos Rodriguez Velez <marcospr@pm.me>
2025-03-20 09:39:05 -0400

    w

--- screen/send/psbtMultisigQRCode.js
+++ screen/send/psbtMultisigQRCode.js
@@ -93,6 +93,7 @@ const PsbtMultisigQRCode = () => {
       testID="PsbtMultisigQRCodeScrollView"
       automaticallyAdjustContentInsets
       contentInsetAdjustmentBehavior="automatic"
+      style={stylesHook.root}
       contentContainerStyle={[styles.scrollViewContent, stylesHook.root, styles.modalContentShort, stylesHook.modalContentShort]}
     >
       <TipBox
