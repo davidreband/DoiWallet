768c81a6b26eda5ba918e5ddd943bbbffee76635
Marcos Rodriguez Vélez <marcospr@pm.me>
2025-03-20 00:39:18 -0400

    Update details.js

--- screen/receive/details.js
+++ screen/receive/details.js
@@ -487,7 +487,7 @@ const ReceiveDetails = () => {
         testID="ReceiveDetailsScrollView"
         contentContainerStyle={[styles.root, stylesHook.root]}
         keyboardShouldPersistTaps="always"
-        onLayout={onLayout} // Apply the onLayout function here
+        onLayout={onLayout}
       >
         <SafeArea style={stylesHook.root}>
           {wallet?.allowBIP47() && wallet?.isBIP47Enabled() && (
@@ -620,7 +620,7 @@ const styles = StyleSheet.create({
   label: {
     fontWeight: '600',
     textAlign: 'center',
-    paddingBottom: 12, // reduced from 24
+    paddingBottom: 12,
   },
   modalButton: {
     paddingVertical: 14,
