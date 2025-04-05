c78996a16cdb6f3c9bf53b5c87ce73f711b37baa
Marcos Rodriguez Velez <marcospr@pm.me>
2025-04-04 23:58:37 -0400

    OPS: RN 77

--- screen/receive/details.js
+++ screen/receive/details.js
@@ -306,10 +306,10 @@ const ReceiveDetails = () => {
   }; 
 
   useEffect(() => {
-    BackHandler.addEventListener('hardwareBackPress', handleBackButton);
+    const subscription = BackHandler.addEventListener('hardwareBackPress', handleBackButton);
 
     return () => {
-      BackHandler.removeEventListener('hardwareBackPress', handleBackButton);
+      subscription.remove();
       clearInterval(fetchAddressInterval.current);
       fetchAddressInterval.current = undefined;
     };
