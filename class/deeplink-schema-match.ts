b1b190715731fc08e2cf972937e4e297e90f9bfd
overtorment <overtorment@gmail.com>
2025-04-07 17:34:23 +0100

    REF: more stuff converted to typescript

--- class/deeplink-schema-match.ts
+++ class/deeplink-schema-match.ts
@@ -430,7 +430,7 @@ class DeeplinkSchemaMatch {
     return bip21.decode(replacedUri, 'doichain');
   }
 
-  static bip21encode(address: string, options: TOptions): string {
+  static bip21encode(address: string, options?: TOptions): string {
     // uppercase address if bech32 to satisfy BIP_0173
     const isBech32 = address.startsWith('dc1');
     if (isBech32) {
