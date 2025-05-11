4d71f767e57b874cfe6272f630c5b17d57905129
Marcos Rodriguez <marcospr@pm.me>
2025-05-11 17:11:38 -0400

    Update deeplink-schema-match.ts

--- class/deeplink-schema-match.ts
+++ class/deeplink-schema-match.ts
@@ -75,7 +75,7 @@ class DeeplinkSchemaMatch {
             ]);
           } else if (action === 'openReceive') {
             completionHandler([
-              'ReceiveDetails',
+              'DetailViewStackScreensStack',
               {
                 screen: 'ReceiveDetails',
                 params: {
